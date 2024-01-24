c51ec7c72f4d39a8cdda7a14e800a990669bb378
Michal K <broomstones-mail@noemail.net>
2024-01-23 23:58:04 -0500

    Rename youtube-test.py to youtube-streaming.py (which is what is deployed anyway)

--- deploy_script.cmd
+++ deploy_script.cmd
@@ -1,7 +1,7 @@
 
 echo Downloading updates from github
 
-curl https://raw.githubusercontent.com/mkarczma/broomstones-streaming/main/youtube-test.py -o youtube-test.py
+curl https://raw.githubusercontent.com/mkarczma/broomstones-streaming/main/youtube-streaming.py -o youtube-streaming.py
 curl https://raw.githubusercontent.com/mkarczma/broomstones-streaming/main/run_script.bat -o run_script.bat
 curl https://raw.githubusercontent.com/mkarczma/broomstones-streaming/main/check_schedule.bat -o check_schedule.bat
 
