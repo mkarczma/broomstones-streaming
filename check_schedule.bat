b0f73cd611f69269e91bab652523b9c32aafd3e4
Michal K <broomstones-mail@noemail.net>
2024-01-23 23:46:02 -0500

    Removing calendar schedule from check_schedule since we just want to check the manual file anyway

--- check_schedule.bat
+++ check_schedule.bat
@@ -1,7 +1,7 @@
 cd C:\Users\techn\Documents\streaming-scripting
 call config.cmd
 
-C:\Users\techn\Documents\streaming-scripting\youtube_api\Scripts\python.exe youtube-streaming.py   --stream_title %stream_title% --sheet %sheet_name% --club_calendar https://broomstones.com/index.php/events/club-calendar --schedule_file manual-schedule.json --only_check_schedule
+C:\Users\techn\Documents\streaming-scripting\youtube_api\Scripts\python.exe youtube-streaming.py   --stream_title %stream_title% --sheet %sheet_name% --schedule_file manual-schedule.json --only_check_schedule
 
 time /t
 pause
