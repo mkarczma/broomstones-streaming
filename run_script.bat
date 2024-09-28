021a71d8389be7c71ec6f105a281021638925f9d
Michal K <broomstones-mail@noemail.net>
2024-09-28 16:11:19 -0400

    adding saturday pickup

--- run_script.bat
+++ run_script.bat
@@ -3,7 +3,8 @@ call config.cmd
 
 
 
-C:\Users\techn\Documents\streaming-scripting\youtube_api\Scripts\python.exe youtube-streaming.py  --yt_oauth_client_secret %secret_filename%  --stream_title %stream_title% --sheet %sheet_name% --obs_path "C:\Program Files\obs-studio\bin\64bit\obs64.exe" --schedule_file manual-schedule.json --club_calendar https://broomstones.com/index.php/calendar/ice-calendar --debug__print_upcoming_calendar_types --broadcast_prefix Derby Presidents Regatta Griffin "Curling McCurlFace" "Chowder Bowl" "Loving Cup" Finlay "Mixed Doubles"
+
+C:\Users\techn\Documents\streaming-scripting\youtube_api\Scripts\python.exe youtube-streaming.py --yt_oauth_client_secret %secret_filename%  --stream_title %stream_title% --sheet %sheet_name% --obs_path "C:\Program Files\obs-studio\bin\64bit\obs64.exe" --schedule_file manual-schedule.json --club_calendar https://broomstones.com/index.php/calendar/ice-calendar --debug__print_upcoming_calendar_types --broadcast_prefix Derby Presidents Regatta Griffin "Curling McCurlFace" "Chowder Bowl" "Loving Cup" Finlay "Mixed Doubles" "Saturday Pickup"
 
 rem C:\Users\techn\Documents\streaming-scripting\youtube_api\Scripts\python.exe youtube-streaming.py  --stream_title %stream_title% --sheet %sheet_name% --obs_path "C:\Program Files\obs-studio\bin\64bit\obs64.exe" --club_calendar https://broomstones.com/index.php/events/club-calendar --debug__print_upcoming_calendar_types --broadcast_prefix Derby Presidents Regatta Griffin "Curling McCurlFace" "Chowder Bowl" "Loving Cup" Finlay
 rem C:\Users\Michal\youtube_api\Scripts\python.exe youtube-test.py --stream_title %stream_title% --sheet A --schedule_file schedule.json --obs_path "C:\Program Files\obs-studio\bin\64bit\obs64.exe" --club_calendar https://broomstones.com/index.php/events/club-calendar --debug__print_upcoming_calendar_types
